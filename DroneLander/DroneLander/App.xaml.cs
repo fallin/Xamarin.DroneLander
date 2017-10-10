7020771f068bfb98811b95cd14665fe9a6564c69
Brant Fallin <brant.fallin@gmail.com>
2017-10-10 14:23:41 -0600

    Added Mobile Center crash and analytics support.

--- DroneLander/DroneLander/App.xaml.cs
+++ DroneLander/DroneLander/App.xaml.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
 using Xamarin.Forms;
+using Microsoft.Azure.Mobile;
+using Microsoft.Azure.Mobile.Analytics;
+using Microsoft.Azure.Mobile.Crashes;
 
 namespace DroneLander
 {
@@ -20,7 +22,9 @@ namespace DroneLander
 
         protected override void OnStart()
         {
-            // Handle when your app starts
+            MobileCenter.Start($"android={Common.MobileCenterConstants.AndroidAppId};" +
+                               $"ios={Common.MobileCenterConstants.iOSAppId}",
+                typeof(Analytics), typeof(Crashes));
         }
 
         protected override void OnSleep()
---
+++ DroneLander/DroneLander/Common/MobileCenterConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace DroneLander.Common
+{
+    public static class MobileCenterConstants
+    {
+        public const string AndroidAppId = "e34b8d4a-336d-4bd3-86d3-8d9db5bc0961";
+        public const string iOSAppId = "";
+    }
+}
